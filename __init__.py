d7a45b72240a7b5bd227f5aa825469eb9c50b6b9
宣源 <xuanyuan.lb@alibaba-inc.com>
2024-08-02 14:17:15 +0800

    fix conflict

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 from .node import SAM2ModelLoader, GroundingDinoModelLoader, GroundingDinoSAMSegment, InvertMask, IsMaskEmptyNode
 
 NODE_CLASS_MAPPINGS = {
-    'SAM2ModelLoader (segment anything)': SAM2ModelLoader,
-    'GroundingDinoModelLoader (segment anything)': GroundingDinoModelLoader,
-    'GroundingDinoSAMSegment (segment anything)': GroundingDinoSAMSegment,
-    'InvertMask (segment anything)': InvertMask,
+    'SAM2ModelLoader (segment anything2)': SAM2ModelLoader,
+    'GroundingDinoModelLoader (segment anything2)': GroundingDinoModelLoader,
+    'GroundingDinoSAMSegment (segment anything2)': GroundingDinoSAMSegment,
+    'InvertMask (segment anything2)': InvertMask,
     "IsMaskEmpty": IsMaskEmptyNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    'SAM2ModelLoader': 'SAM2 Model Loader',
-    'GroundingDinoModelLoader': 'Grounding Dino Model Loader',
-    'GroundingDinoSAMSegment': 'Grounding Dino SAM Segment',
-    'InvertMask': 'Invert Mask',
+    'SAM2ModelLoader (segment anything2)': 'SAM2 Model Loader (segment anything2)',
+    'GroundingDinoModelLoader (segment anything2)': 'Grounding Dino Model Loader (segment anything2)',
+    'GroundingDinoSAMSegment (segment anything2)': 'Grounding Dino SAM Segment (segment anything2)',
+    'InvertMask (segment anything2)': 'Invert Mask (segment anything2)',
     "IsMaskEmpty": "Is Mask Empty",
 }
